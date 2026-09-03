10c68ec702a77c70c5609de0992f5a0a11abd77d
Balka <martin.amoreau@hotmail.fr>
2026-09-03 07:32:23 +0200

    wash

--- src/001_main/001_main.c
+++
@@ -1,8 +0,0 @@
-#include "../../include/cub3d.h"
-
-int main (int argc, char **argv)
-{
-    // check la longueur du path pour éviter les fourberies
-    if(check_args(argc, argv))
-        return (1);
-}
